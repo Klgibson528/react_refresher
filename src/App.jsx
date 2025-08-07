9ceab3b38a62851517058a8c479778ed622acddd
klgibson528 <klgibson528@gmail.com>
2025-08-07 10:05:31 +0200

    improving file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core', 'Essential', 'Basic', 'Important'];
-
-function getRandomDescription() {
-  const randomIndex = Math.floor(Math.random() * reactDescriptions.length);
-  return reactDescriptions[randomIndex];
-}
-function Header(){
-  const description = getRandomDescription();
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-      <ul>
-
-      </ul>
-    </header> 
-  )
-}
-
-function CoreConcept(props){
-  return (
-    <li>  
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>  
-    </li>
-
-)}
 
 function App() {
   return (
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import reactImg from '../assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core', 'Essential', 'Basic', 'Important'];
+
+export default function getRandomDescription() {
+  const randomIndex = Math.floor(Math.random() * reactDescriptions.length);
+  return reactDescriptions[randomIndex];
+}
+function Header(){
+  const description = getRandomDescription();
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+      <ul>
+
+      </ul>
+    </header> 
+  )
+}
